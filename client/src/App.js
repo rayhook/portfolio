d7dc66246fa4cce614401062ddb8d15555ddb9ea
rayhook <koohyar@gmail.com>
2022-03-18 15:22:07 +0100

    Add placeholder design for main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./App.css";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="container">
+      <div className="title">Portfolio</div>
+    </div>
+  );
 }
 
 export default App;
