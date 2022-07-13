0e229e24781d551712233d5b614938d0e08f04a9
rayhook <koohyar@gmail.com>
2022-07-13 17:59:16 +0200

    Add new form inputs

--- client/src/Components/Form.js
+++ client/src/Components/Form.js
@@ -5,9 +5,10 @@ const Form = () => {
     first_name: "",
     last_name: "",
     age: "",
-    investment: "--Select an option---",
+    investment: "",
     options: ["public stocks", "venture", "angel"],
-    fastProcess: false
+    contact: [],
+    options2: ["CV", "coverletter", "personal project"]
   });
 
   const handleSubmit = (e) => {
@@ -17,49 +18,59 @@ const Form = () => {
 
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
-    console.log(type, name);
+    console.log("type: ", type, "name: ", name, "value: ", value, "checked: ", checked);
 
     const isSelect = type === "select-one";
     const isCheckbox = type === "checkbox";
     if (isSelect) {
       setFormData({ ...formData, investment: value });
     } else if (isCheckbox) {
-      setFormData({ ...formData, [name]: checked });
+      if (checked) {
+        setFormData({ ...formData, contact: [...formData.contact, name] });
+      } else {
+        setFormData({
+          ...formData,
+          contact: formData.contact.filter((val) => val !== name)
+        });
+      }
     } else {
       setFormData({ ...formData, [name]: value });
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <TextInput
-        label="First Name"
-        name="first_name"
-        value={formData.first_name}
-        handleChange={handleChange}
-      />
-      <TextInput
-        label="Last Name"
-        name="last_name"
-        value={formData.last_name}
-        handleChange={handleChange}
-      />
-      <NumberInput label="Age" name="age" value={formData.age} handleChange={handleChange} />
-      <MultiSelect
-        label="Specialty"
-        name="options"
-        value={formData.investment}
-        handleChange={handleChange}
-        options={formData.options}
-      />
-      <RadioInput
-        label="Fast proceess"
-        name="fastProcess"
-        value={formData.fastProcess}
-        handleChange={handleChange}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <div className="wrapper">
+      <form onSubmit={handleSubmit}>
+        <TextInput
+          label="First Name"
+          name="first_name"
+          value={formData.first_name}
+          handleChange={handleChange}
+        />
+        <TextInput
+          label="Last Name"
+          name="last_name"
+          value={formData.last_name}
+          handleChange={handleChange}
+        />
+        <NumberInput label="Age" name="age" value={formData.age} handleChange={handleChange} />
+        <MultiSelect
+          label="Specialty"
+          name="options"
+          value={formData.investment}
+          handleChange={handleChange}
+          options={formData.options}
+        />
+        <RadioInput
+          label="Fast proceess"
+          name="contact"
+          formData={formData}
+          handleChange={handleChange}
+        />
+        <Checkbox label="documents" options={formData.options2} handleChange={handleChange} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 };
 
@@ -95,12 +106,29 @@ const MultiSelect = ({ label, name, value, options, handleChange }) => {
   );
 };
 
-const RadioInput = ({ label, name, value, handleChange }) => {
+const RadioInput = ({ label, name, formData, handleChange }) => {
   return (
     <fieldset>
-      <label>{label}</label>
-      <input type="radio" name="option1" checked={value} onChange={handleChange} />
-      <input type="radio" name="option2" checked={value} onChange={handleChange} />
+      <label>TV</label>
+      <input type="radio" name={name} value="TV" onChange={handleChange} />
+      <label>Sat</label>
+      <input type="radio" name={name} value="Sat" onChange={handleChange} />
+      <label>Goggles</label>
+      <input type="radio" name={name} value="Goggle" onChange={handleChange} />
+    </fieldset>
+  );
+};
+
+const Checkbox = ({ label, options, handleChange }) => {
+  return (
+    <fieldset>
+      <h2>{label}</h2>
+      {options.map((option) => (
+        <div key={option}>
+          <label>{option} </label>
+          <input type="checkbox" name={option} onChange={handleChange} />
+        </div>
+      ))}
     </fieldset>
   );
 };
